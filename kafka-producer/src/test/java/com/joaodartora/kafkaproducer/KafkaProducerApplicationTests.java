76f6818762983855997fce5244160528b7a4d9d3
João Dartora <joao.dartora@ilegra.com>
2020-07-02 00:41:20 -0300

    feat: adding kafka producer poc structure

---
+++ kafka-producer/src/test/java/com/joaodartora/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodartora.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
