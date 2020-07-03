028e733c3a5418da1b41d3c54e334f4711db42e0
João Dartora <joao.dartora@ilegra.com>
2020-07-03 00:13:54 -0300

    refactor: simplifying java producer demo

---
+++ kafka-producer/src/main/java/ProducerDemo.java
@@ -0,0 +1,27 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        String boostrapServers = "localhost:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("test-topic", "testing kafka producer");
+
+        producer.send(record);
+        producer.close();
+    }
+
+}
